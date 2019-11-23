d958be39d7c64a9b46b4a4b792a6c5b2f32bf182
olive <daniellek@mobilefuse.com>
2019-11-22 20:14:55 -0500

    added a bunch in JS

--- liri.js
+++ liri.js
@@ -1,23 +1,111 @@
 require("dotenv").config();
+
 let keys = require("./keys.js");
+let Spotify = require('node-spotify-api'); //Using the Spotify api and getting the key from keys.js
 let spotify = new Spotify(keys.spotify);
 
-
-const moment = require('moment'); //instructions call for bands in twon to use node
+let moment = require('moment'); //Both required to use moment for node
 moment().format();
-const axios = require('axios'); //info for APIs movie and concert-this
 
-const fs = require('fs'); //Node file share needed
-// Write statement so liri.js can take in one of the following commands:
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
+let axios = require('axios'); //To get the information from the APIs for movie and concert-this
 
-//write function for concert-this
+let fs = require('fs'); //To read the random.txt file for the do-what-it-says function
 
-//write function for spotify-this-song
+let command = process.argv[2]; //For the switch statement
+let value = process.argv[3]; //To send the song/movie/concert to their respective functions
 
-//write function for movie-this
+switch (command) {
+    case "concert-this":
+        concertThis(value);
+        break;
+    case "spotify-this-song":
+        spotifySong(value);
+        break;
+    case "movie-this":
+        movieThis(value);
+        break;
+    case "do-what-it-says":
+        doThis(value);
+        break;
+};
 
-//write function for do-what-it-says
+function concertThis(value) {
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
+    .then(function(response) {    
+        for (let i = 0; i < response.data.length; i++) {
+
+            let datetime = response.data[i].datetime; //Saves datetime response into a variable
+            let dateArr = datetime.split('T'); //Attempting to split the date and time in the response
+
+            let concertResults = 
+                "--------------------------------------------------------------------" +
+                    "\nVenue Name: " + response.data[i].venue.name + 
+                    "\nVenue Location: " + response.data[i].venue.city +
+                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY"); //dateArr[0] should be the date separated from the time
+            console.log(concertResults);
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+        
+
+}
+
+function spotifySong(value) {
+    if(!value){
+        value = "The Sign";
+    }
+    spotify
+    .search({ type: 'track', query: value })
+    .then(function(response) {
+        for (let i = 0; i < 5; i++) {
+            let spotifyResults = 
+                "--------------------------------------------------------------------" +
+                    "\nArtist(s): " + response.tracks.items[i].artists[0].name + 
+                    "\nSong Name: " + response.tracks.items[i].name +
+                    "\nAlbum Name: " + response.tracks.items[i].album.name +
+                    "\nPreview Link: " + response.tracks.items[i].preview_url;
+                    
+            console.log(spotifyResults);
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
+
+function movieThis(value) {
+    if(!value){
+        value = "mr nobody";
+    }
+    axios.get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy")
+    .then(function(response) {
+             movieResults = 
+                "--------------------------------------------------------------------" +
+                    "\nMovie Title: " + response.data.Title + 
+                    "\nYear of Release: " + response.data.Year +
+                    "\nIMDB Rating: " + response.data.imdbRating +
+                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                    "\nCountry Produced: " + response.data.Country +
+                    "\nLanguage: " + response.data.Language +
+                    "\nPlot: " + response.data.Plot +
+                    "\nActors/Actresses: " + response.data.Actors;
+            console.log(movieResults);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    
+}
+
+function doThis(value) {
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        let dataArr = data.split(',');
+        spotifySong(dataArr[0], dataArr[1]);
+    })
+}
